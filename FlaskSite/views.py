39f6c473720a3253f0ae4230e6cb0336e1fb7a4b
Jackson Peters <jpete170@gmail.com>
2021-11-23 18:56:04 +1000

    Basic additions, changes made to about, base and index html pages

--- FlaskSite/views.py
+++ FlaskSite/views.py
@@ -2,10 +2,10 @@ from flask import Blueprint, render_template, url_for, redirect
 
 bp = Blueprint('main', __name__)
 
-@bp.route('/', methods=['GET', 'POST'])
+@bp.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
 
-@bp.route('/about', methods=['GET', 'POST'])
+@bp.route('/about', methods=['GET'])
 def about():
     return render_template('about.html')
