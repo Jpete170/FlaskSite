8e9cabf6fee7c84d52c9ba656d82cbf6078241d8
Jackson <jpete170@gmail.com>
2021-11-23 15:33:18 +1000

    added some bootstrap elements, began to structure the web app out (added an 'about.html' file, to be expanded upon later)

--- FlaskSite/views.py
+++ FlaskSite/views.py
@@ -5,3 +5,7 @@ bp = Blueprint('main', __name__)
 @bp.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
+
+@bp.route('/about', methods=['GET', 'POST'])
+def about():
+    return render_template('about.html')
