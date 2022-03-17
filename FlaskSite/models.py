3d36876d6a0af4050a87d19a10a554354b2bb0df
Jpete170 <39613600+Jpete170@users.noreply.github.com>
2022-03-17 11:50:39 +1000

    updated db and models.py

--- FlaskSite/models.py
+++ FlaskSite/models.py
@@ -7,6 +7,14 @@ from . import db
 #Genres will be used as part of a future functionality
 class genres(enum.Enum):
     Science_Fiction = 1
+    Romance = 2
+    Fiction = 3
+    Drama = 4
+
+#not yet implemented in the database
+#implemented here for future implementation reference
+class sub_genres(enum.Enum):
+    Marriage_Drama= 1
 
 
 class Item(db.Model):
@@ -17,7 +25,8 @@ class Item(db.Model):
     Author = db.Column(db.String(100), index=True)
     Description = db.Column(db.String(250), index=True)
     Genre = db.Column(db.Enum(genres))
-
+    bookURL = db.column(db.String(100), index=True)
+    bookIMG = db.column(db.String(100), index=True)
     def __repr__(self):
         return "<Name: {}, ID: {}".format(self.title, self.id)
 
