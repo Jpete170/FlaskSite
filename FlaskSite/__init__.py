32ec8ac998f54c532da81c28a0a61402ad69aac1
Jackson Peters <jackson1480@hotmail.com>
2025-12-28 15:37:09 +1000

    change init for vercel

--- FlaskSite/__init__.py
+++ FlaskSite/__init__.py
@@ -7,29 +7,26 @@ from flask_migrate import Migrate
 
 db = SQLAlchemy()
 
-def create_app():
-    app= Flask(__name__)
-    app.debug = True
-    app.secret_key='utroutoru'
 
-    Bootstrap(app)
-    folder = os.path.dirname(os.path.abspath(__file__))
-    #location = os.path.join(folder, 'books.db')
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///{}/books.db'.format(folder)
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    migrate = Migrate(app, db)
-    db.init_app(app)
-    
-    #view stuff, to avoid circular references
-    from . import views
-    app.register_blueprint(views.bp)
+app= Flask(__name__)
+app.debug = True
+app.secret_key='utroutoru'
+Bootstrap(app)
+folder = os.path.dirname(os.path.abspath(__file__))
+#location = os.path.join(folder, 'books.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///{}/books.db'.format(folder)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+migrate = Migrate(app, db)
+db.init_app(app)
 
-    @app.errorhandler(404)
-    def page_not_found(e):
-        return render_template('errors/404.html'), 404
+#view stuff, to avoid circular references
+from . import views
+app.register_blueprint(views.bp)
 
-    @app.errorhandler(500)
-    def internal_server_error(e):
-        return render_template('errors/500.html'), 500
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('errors/404.html'), 404
 
-    return app
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('errors/500.html'), 500
